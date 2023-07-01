01e123c0c0d451294d69afaac6635b32dd4d6e1c
mlimaicof <mlimaico@icloud.com>
2023-06-30 19:10:55 -0500

    Creacion de proyecto

---
+++ src/main/java/com/privado/fame/FameApplication.java
@@ -0,0 +1,13 @@
+package com.privado.fame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/privado/fame/FameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.privado.fame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
